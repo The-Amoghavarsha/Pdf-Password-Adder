0279ab08b8b9b241d818e2c21f7dc4d169f6e635
Amoghavarsha <35860305+The-Amoghavarsha@users.noreply.github.com>
2025-01-22 13:38:41 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email='amoghavarsha004@gmail.com',
     license='MIT',
     packages=find_packages(),
-    install_requires=["qpdf"],
+    install_requires=[],
     entry_points={
         'console_scripts': [
             'pdf_password_adder = pdf_password_adder.pdf_password_adder:main'
